056b545db8538e6bdb23d5e64c64db71a4e0ba25
paulisturm <paulisturm22@gmail.com>
2023-09-28 22:30:32 -0400

    pages and bootstrap

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+    
+    <Navbar/>
+    <Routes>
+          <Route path="/" element={<Home />} />
+          
+          
+       </Routes>
+    </>
+  );
+}
+
+export default App;

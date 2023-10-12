2a9f868fd842061ce58ef8f78db9b5bba56536f5
paulisturm <paulisturm22@gmail.com>
2023-10-12 19:04:42 -0400

    projects page

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 function Projects () {
     return (
-        <>
-        pee
-        </>
+        <body>
+  <div class="container mt-4">
+    <div class="row">
+      
+      <div class="col-md-4">
+        <img src="project1.jpg" alt="Project 1" class="img-fluid"/>
+      </div>
+      <div class="col-md-8">
+        <h2>Project 1</h2>
+        <p>This is a description of the first project. You can provide details about the project, the technologies used, and its significance.</p>
+      </div>
+
+      
+      <div class="col-md-4">
+        <img src="project2.jpg" alt="Project 2" class="img-fluid"/>
+      </div>
+      <div class="col-md-8">
+        <h2>Project 2</h2>
+        <p>Here goes the description of the second project. Explain what it is, and why it's important.</p>
+      </div>
+
+      <div class="col-md-4">
+        <img src="project3.jpg" alt="Project 3" class="img-fluid"/>
+      </div>
+      <div class="col-md-8">
+        <h2>Project 3</h2>
+        <p>This is the description of the third project. Highlight the key features and accomplishments of this project.</p>
+      </div>
+    </div>
+  </div>
+</body>
     )
 
 }
